435f3a34576d01a7f6144ff25c4605bf7a0ba5da
Olivervesth <48321388+Olivervesth@users.noreply.github.com>
2021-08-13 08:31:34 +0200

    Add files via upload

---
+++ Milk and Cookies/Milk and Cookies/Controllers/Shoppingcart.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace sessions_og_cookies.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class Shoppingcart : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{id}/{productname}/{price}")]
+        public string AddProduct(int id, string productname, int price)
+        {
+            Product newp = new Product();//Making new product object and fills it with the parameters
+            newp.id = id;
+            newp.name = productname;
+            newp.price = price;
+            List<Product> plist = new List<Product>();
+
+            plist.Add(newp);
+            string jsonstring = "";
+            string cart = HttpContext.Session.GetString("shoppingcart");//lets get the data thats in the session
+            if (cart != null)
+            {
+                List<Product> cartlist = JsonSerializer.Deserialize<List<Product>>(cart);//We are Deserializing the jsonstring we got from session transforming it into a list of products
+                plist.AddRange(cartlist);
+                jsonstring = JsonSerializer.Serialize(plist);
+            }
+            else
+            {
+                jsonstring = JsonSerializer.Serialize(plist);
+            }
+
+            Request.HttpContext.Session.SetString("shoppingcart", jsonstring);//Set new data in session
+            return "saved";
+        }
+
+        [HttpGet]
+        [Route("shoppingcart")]
+        public string GetProducts()
+        {
+            string cartstring = HttpContext.Session.GetString("shoppingcart");
+            if (cartstring != null)
+            {
+                return cartstring;//Return the data in session
+            }
+            else
+            {
+                return "Your kurv is empty!";
+            }
+
+
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public string DeleteProduct(int id)
+        {
+            List<Product> cartlist = JsonSerializer.Deserialize<List<Product>>(HttpContext.Session.GetString("shoppingcart"));
+
+            for (int i = 0; i < cartlist.Count; i++)//Compares all the ids in the cart with the one you wanna delete
+            {
+                if (cartlist[i].id == id)
+                {
+                    string delitem = cartlist[i].name;
+                    cartlist.Remove(cartlist[i]);//remove item from cart
+                    Request.HttpContext.Session.SetString("shoppingcart", JsonSerializer.Serialize(cartlist));//Send back the cart with the removed item
+                    return "Deleted :" + delitem;
+
+                }
+            }
+
+            return "No item with that id in your cart!";
+
+        }
+    }
+}
---
+++ Milk and Cookies/Milk and Cookies/Controllers/SodaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace sessions_og_cookies.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SodaController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("{item}")]
+        public string Get(string item )
+        {
+            DateTimeOffset dt = DateTimeOffset.Now.AddMinutes(5); // create a datetime object so we can add 5 min to the cookie lifespand
+            CookieOptions co = new CookieOptions();
+            co.Expires = dt;
+            Response.Cookies.Append("favoriteSoda", item,co);
+            //Response.Cookies.Append("favoriteSoda", item, co);
+            return "fav: " + item;
+        }
+
+        [HttpGet]
+        [Route("favoriteSoda")]
+        public string GetCookie()
+        {
+            return Request.Cookies["favoriteSoda"];
+        }
+
+        [HttpGet]
+        [Route("setcookie/{type}")]
+        public void SetMyCookie(string type)
+        {
+            Response.Cookies.Append("favoriteCookie", type);//make tha cookie
+        }
+
+        [HttpGet]
+        [Route("getcookie")]
+        public string GetMyCookie()
+        {
+            return Request.Cookies["favoriteCookie"];//Eat the cookie og get it at least
+        }
+
+    }
+}
